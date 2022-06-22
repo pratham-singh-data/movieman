5d397f76f89d13605a7a40e09aa11edbaae5e604
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-06-22 19:28:48 +0530

    Refactored Search and Displayed Results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
 import React, {useState} from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import apiGet from "../misc/config";
 
 const Home = () =>{
     const [input, setInput] = useState("");
+    const [results, setResults] = useState(null);
 
     const onInputChange = (ev) => {
         setInput(ev.target.value);
     }
 
     const onSearch = () => {
-        // https://api.tvmaze.com/search/shows?q=air
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(res => res.json()).then(result => {
-            console.log(result);
-        })
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
+        });
     }
 
     const onKeyDownInput = (ev) => {
@@ -21,10 +22,35 @@ const Home = () =>{
         }
     }
 
+    const renderResults = () => {
+        if(results && results.length === 0){
+            // serached for something but got nothing
+            return(
+                <div>
+                    No results found
+                </div>
+            );
+        }
+
+        if(results && results.length > 0){
+            // got some results
+            return(
+                <div>
+                    {results.map((item) => <div key={item.show.id}>{item.show.name}</div> )}
+                </div>
+            )
+        }
+
+        // else
+        return null;
+    }
+
     return(
         <MainPageLayout>
             <input type="text" onKeyDown={(ev) => onKeyDownInput(ev)} onChange={(ev) => onInputChange(ev)}/>
             <button type="button" onClick={(ev) => onSearch()}>Search</button>
+
+            {renderResults()}
         </MainPageLayout>
     );
 }
