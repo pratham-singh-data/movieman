5e390055227fb8bd97b04c18f12a82db57e586e0
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-06-23 07:03:14 +0530

    Cards Added

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ActorCard from "./ActorCard";
 import IMAGE_NOT_FOUND from "../../images/not-found.png";
+import {FlexGrid} from "../styled";
 
 const ActorGrid = ({data}) => {
     return(
-        <div>
+        <FlexGrid>
             {
                 data.map(({person}) => (<ActorCard key={person.id} name={person.name} gender={person.gender} country={person.country ? person.country.name : null} birthday={person.birthday} deathday={person.deathday} image={person.image?person.image.medium:IMAGE_NOT_FOUND}/>))
             }
-        </div>
+        </FlexGrid>
     );
 }
 
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ShowCard from "./ShowCard";
 import IMAGE_NOT_FOUND from "../../images/not-found.png"
+import {FlexGrid} from "../styled";
 
 const ShowGrid = ({data}) => {
-    return(<div>
+    return(<FlexGrid>
         {
-            data.map(({show}) => <ShowCard key={show.id} name={show.name} image={show.name?show.image.medium:IMAGE_NOT_FOUND} summary={show.summary}/>)
+            data.map(({show}) => <ShowCard key={show.id} name={show.name} image={show.image?show.image.medium:IMAGE_NOT_FOUND} summary={show.summary}/>)
         }
-    </div>);
+    </FlexGrid>);
 }
 
 export default ShowGrid;
