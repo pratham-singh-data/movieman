ff696e4229ebf535ca085e156feded7e126e9675
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-06-23 07:52:26 +0530

    Loading And Error Pages Added

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,16 +5,42 @@ import apiGet from "../misc/config";
 const Show = () => {
     const {id} = useParams();
     const [show, setShow] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
+        let isMounted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(res => {
-            setShow(res);
+            if(isMounted){
+                setShow(res);
+                setLoading(false);
+            }
+        }).catch((err) => {
+            setError(err.message);
+            setLoading(false);
         });
+
+        return () => {
+            isMounted = false;
+        }
     }, [id]);
 
     useEffect(() => {
         console.log(show);
     }, [show]);
+
+    if(loading){
+        return(
+            <div>Loading Data</div>
+        );
+    }
+
+    if(error){
+        return(
+            <div>Error Occurred</div>
+        );
+    }
 
     return(
         <div>
