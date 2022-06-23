6b4c0abc2a74d0f568980c6b6b31f90476824ded
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-06-23 09:08:17 +0530

    Transition Added

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -6,7 +6,7 @@ import {FlexGrid} from "../styled";
 const ShowGrid = ({data}) => {
     return(<FlexGrid>
         {
-            data.map(({show}) => <ShowCard key={show.id} name={show.name} image={show.image?show.image.medium:IMAGE_NOT_FOUND} summary={show.summary}/>)
+            data.map(({show}) => <ShowCard key={show.id} id={show.id} name={show.name} image={show.image?show.image.medium:IMAGE_NOT_FOUND} summary={show.summary}/>)
         }
     </FlexGrid>);
 }
